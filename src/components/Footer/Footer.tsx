11ee1c3aa7695fb8bd1b5fb82fe053a74a19c1f6
blessme247 <solabiabraham@gmail.com>
2023-11-16 14:30:15 +0100

    Created optimized production build

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -51,7 +51,7 @@ const Footer = () => {
 
         <div className={footerMiddle}>
           <p>
-            Audiophile is an all in one stop to fulfill your audio needs. We're
+            Audiophile is an all in one stop to fulfill your audio needs. We’re
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
             our demo facility - we’re open 7 days a week.
